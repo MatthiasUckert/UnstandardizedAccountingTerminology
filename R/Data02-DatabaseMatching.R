64d7885704ae69e26cc7cab9a52149f11bc553b3
MatthiasUckert <matthias.uckert@outlook.com>
2025-10-23 17:25:46 +0200

    Update

---
+++ R/Data02-DatabaseMatching.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+
+arrow::read_parquet("data-raw/DataBaseMatching.parquet") %>% 
+  dplyr::distinct(Database, DatabaseVar, TagMod) %>% 
+  dplyr::filter(!is.na(TagMod)) %>% 
+  dplyr::filter(!TagMod == "NA") %>% 
+  dplyr::group_by(Database, DatabaseVar) %>% 
+  dplyr::summarise(TagMod = paste(TagMod, collapse = "\n"), .groups = "drop") %>% 
+  openxlsx::write.xlsx("excel-raw/DatabaseAndTags.xlsx")
+
+
+arrow::read_parquet("data-raw/DataBaseMatching.parquet") %>% 
+  dplyr::distinct(Termlist, Database, DatabaseVar, CID) %>% 
+  dplyr::group_by(Termlist, Database, DatabaseVar) %>% 
+  dplyr::summarise(CID = paste(CID, collapse = "\n"), .groups = "drop") %>% 
+  tidyr::pivot_wider(names_from = Termlist, values_from = CID) %>% 
+  openxlsx::write.xlsx("excel-raw/DatabaseAndCIDs.xlsx")
+
+
+dplyr::bind_rows(
+  openxlsx::read.xlsx("downloads/DatabaseConceptMatching.xlsx", 1) %>% 
+    tibble::as_tibble() %>% 
+    dplyr::mutate(dplyr::across(
+      .cols = c(Item.Description, X5, X6, X7, X8, X9),
+      .fns = ~ dplyr::if_else(is.na(.), "  ", .)
+    )) %>% 
+    tidyr::unite(ItemDescription, c(Item.Description, X5, X6, X7, X8, X9), sep = "") %>% 
+    dplyr::mutate(Database = "Worldscope", .before = Panel),
+  
+  
+  openxlsx::read.xlsx("downloads/DatabaseConceptMatching.xlsx", 2) %>% 
+    tibble::as_tibble() %>% 
+    dplyr::mutate(dplyr::across(
+      .cols = c(Item.Description, X5, X6, X7),
+      .fns = ~ dplyr::if_else(is.na(.), "  ", .)
+    )) %>% 
+    tidyr::unite(ItemDescription, c(Item.Description, X5, X6, X7), sep = "") %>% 
+    dplyr::mutate(Database = "Compustat", .before = Panel)
+) %>% arrow::write_parquet("data/DatabaseMatching.parquet")
+
